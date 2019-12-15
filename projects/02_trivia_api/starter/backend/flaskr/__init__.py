e9ff1276a494014696e3839951cf55cb42031ea5
Stephan Lefort <lefortsge@gmail.com>
2019-12-15 13:31:42 -0800

    Implemented Todos for TriviaApi

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -8,25 +8,50 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, list):
+  page = request.args.get('page', 1, type=int)
+  start = (page - 1) * QUESTIONS_PER_PAGE
+  end = start + QUESTIONS_PER_PAGE
+
+  questions = [question.format() for question in list]
+  actual_questions = questions[start:end]
+
+  return actual_questions
+
+
 def create_app(test_config=None):
-  # create and configure the app
+  #create and configure the app
   app = Flask(__name__)
   setup_db(app)
+  
   
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
-
+  CORS(app, resources={r"/api/*":{"origins": "*"}})
+  
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
-
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-ALlow-Headers', 'GET, POST, DELETE')
+    return response
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
-
+  @app.route('/categories', methods=['GET'])
+  def get_categories():
+    categories = Category.query.order_by(Question.id).all()
+    formatted_categories = [category.format() for category in categories]
+    
+    return jsonify({
+      'success': True,
+      'categories': formatted_categories
+    })
 
   '''
   @TODO: 
@@ -40,6 +65,22 @@ def create_app(test_config=None):
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+  @app.route('/questions', methods=['GET'])
+  def get_questions():
+    questions = Question.query.order_by(Question.id).all()
+    current_questions = paginate_questions(request, questions)
+    num_of_questions = len(questions)
+    
+    categories = Category.query.all()
+    formatted_categories = [category.format() for category in categories]
+    
+    return jsonify({
+      'success': True,
+      'questions': current_questions,
+      'total_questions': num_of_questions,
+      'categories': formatted_categories,
+      'current_category': ''
+    })
 
   '''
   @TODO: 
@@ -48,7 +89,28 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    try:
+      question = Question.query.filter(Question.id == question_id).one_or_none()
 
+      if question is None:
+        abort(404)
+      
+      question.delete()
+      remaining_questions = Question.query.order_by(Question.id).all()
+      questions = paginate_questions(request, remaining_questions)
+      num_of_questions = len(remaining_questions)
+      return jsonify({
+        'success': True,
+        'deleted': question_id,
+        'questions':questions,
+        'total_questions':num_of_questions
+      })
+
+    except:
+      abort(422)
+      
   '''
   @TODO: 
   Create an endpoint to POST a new question, 
@@ -59,7 +121,29 @@ def create_app(test_config=None):
   the form will clear and the question will appear at the end of the last page
   of the questions list in the "List" tab.  
   '''
+  @app.route('/questions', methods=['POST'])
+  def add_question():
+    body = request.get_json()
 
+    new_question = body.get('question', None)
+    new_answer = body.get('answer', None)
+    new_category = body.get('category', None)
+    new_difficulty = body.get('difficulty', None)
+
+    try:
+      question = Question(question=new_question, answer=new_answer, category=new_category, difficulty=new_difficulty)
+      question.insert()
+
+      questions = Question.query.order_by(Question.id).all()
+      current_questions = paginate_questions(request, questions)
+      num_of_questions = len(questions)
+
+      return jsonify({
+        'success': True,
+      })
+    
+    except:
+      abort(422)
   '''
   @TODO: 
   Create a POST endpoint to get questions based on a search term. 
@@ -70,6 +154,27 @@ def create_app(test_config=None):
   only question that include that string within their question. 
   Try using the word "title" to start. 
   '''
+  @app.route('/questions', methods=['POST'])
+  def search_questions():
+    body = request.get_json()
+    search = body.get('search', None)
+
+    try:
+      if search:
+        questions = Question.query.order_by(Question.id).filter(Question.question.ilike('%{}%'.format(search)))
+        current_questions = paginate_questions(request, questions)
+        num_of_questions = len(current_questions)
+
+        return jsonify({
+          "success": True,
+          'questions': current_questions,
+          'total_questions': num_of_questions
+        })
+      else:
+        abort(404)
+    except:
+      abort(422)
+
 
   '''
   @TODO: 
@@ -98,7 +203,38 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
-  
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+      "success": False,
+      "error": 400,
+      "message": "Bad Request"
+    }), 400
+
+  @app.errorhandler(404)
+  def non_found(error):
+    return jsonify({
+      "success": False,
+      "error": 404,
+      "message": "Resource Not Found"
+    }), 404
+
+  @app.errorhandler(405)
+  def not_allowed(error):
+    return jsonify({
+      "success": False,
+      "error": 405,
+      "message": "Method Not Allowed"
+    })
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+      "success": False,
+      "error": 422,
+      "message": "Unprocessable"
+    }), 422
+
   return app
 
     
